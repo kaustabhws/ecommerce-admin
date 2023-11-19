ca2b1bacc5da2e7d1926bdb402a1ceca17b68ab2
Kaustabh Mahanta <kaustabhworkspace@gmail.com>
2023-11-19 15:41:23 +0530

    updated dashboard and routes

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,16 +1,16 @@
 "use client";
 
+import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { OrderColumn, columns } from "./columns";
-import { DataTable } from "@/components/ui/data-table";
+
+import { columns, OrderColumn } from "./columns";
 
 interface OrderClientProps {
   data: OrderColumn[];
 }
 
 export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
-
   return (
     <>
       <Heading
@@ -18,7 +18,7 @@ export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
         description="Manage orders for your store"
       />
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="label" />
+      <DataTable searchKey="products" columns={columns} data={data} />
     </>
   );
 };
--- app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -1,21 +1,26 @@
-"use client"
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
+import { ColumnDef } from "@tanstack/react-table";
 
 export type OrderColumn = {
-  id: string
+  id: string;
+  email: string;
   phone: string;
   address: string;
   isPaid: boolean;
   totalPrice: string;
-  products: string
+  products: string;
   createdAt: string;
-}
+};
 
 export const columns: ColumnDef<OrderColumn>[] = [
   {
     accessorKey: "products",
     header: "Products",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
   },
   {
     accessorKey: "phone",
@@ -27,7 +32,7 @@ export const columns: ColumnDef<OrderColumn>[] = [
   },
   {
     accessorKey: "totalPrice",
-    header: "Total Price",
+    header: "Total price",
   },
   {
     accessorKey: "isPaid",
